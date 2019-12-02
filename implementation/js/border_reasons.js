0edbf054682fa0fdcc959e22487f31bc0e7a8188
jovinleong <jovinwleong@gmail.com>
2019-12-02 16:16:51 -0500

    some donut implementation

--- implementation/js/border_reasons.js
+++ implementation/js/border_reasons.js
@@ -28,9 +28,10 @@ borderReason.prototype.initVis = function() {
     vis.filteredData = vis.data;
 
     // set the dimensions and margins of the graph
-        vis.margin = {top: 20, right: 20, bottom: 30, left: 50},
+        vis.margin = {top: 20, right: 20, bottom: 30, left: 50};
         vis.width = 670 - vis.margin.left - vis.margin.right;
         vis.height = 300 - vis.margin.top - vis.margin.bottom;
+        vis.radius = Math.min(width, height)/4;
 
     // Add an SVG element with the desired dimensions and margin.
     vis.svg = d3.select("#" + vis.parentElement).append("svg")
@@ -45,6 +46,24 @@ borderReason.prototype.initVis = function() {
         .range([0, vis.width]);
     vis.y = d3.scaleLinear()
         .range([vis.height, 0]);
+
+    vis.donutColor = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"]);
+
+    vis.donutChart = d3.pie()
+        .sort(null)
+        .value(function (d) {
+            return vis.x(d)
+        });
+
+    vis.donutPath = d3.arc()
+        .outerRadius(vis.radius - 10)
+        .innerRadius(vis.radius - 70);
+
+    vis.donutLabel = d3.arc()
+        .outerRadius(vis.radius - 40)
+        .innerRadius(vis.radius - 40);
+
+
 
     vis.wrangleData();
 };
@@ -85,6 +104,15 @@ borderReason.prototype.updateVis = function() {
     // Update domain
     vis.x.domain(d3.extent(nestedData, function(d) { return  d; }));
     vis.y.domain(nestedTitles.map(function(d) { return  d; }));
+
+    vis.arc = g.selectAll(".arc")
+        .data(vis.donutChart(nestedData))
+        .enter()
+        .append("g")
+        .attr("class", "arc");
+
+
+
 
     // console.log("new data", nestedData);
     // Update y-axis and labels
@@ -140,7 +168,6 @@ borderReason.prototype.updateVis = function() {
 
 borderReason.prototype.selectionChange = function(brushRegion){
     var vis = this;
-    console.log("does it work")
     // Filter data based on selection range with areachart's x scale
     vis.filteredData = vis.data.filter(function (value) {
         return (border_years.x(new Date(value.Established)) >= brushRegion[0]) && (border_years.x(new Date(value.Removed)) <= brushRegion[1])
